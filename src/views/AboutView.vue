ed1a8b36217cd54ff7555959d6d5689a8340568b
MichaelaTarryn <mward7006@gmail.com>
2022-09-27 15:20:01 +0200

    first app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about pa-5">
+    <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+       Ex necessitatibus praesentium sint quis exercitationem, labore quisquam qui magnam laboriosam provident.</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+       Ex necessitatibus praesentium sint quis exercitationem, labore quisquam qui magnam laboriosam provident.</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+       Ex necessitatibus praesentium sint quis exercitationem, labore quisquam qui magnam laboriosam provident.</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+       Ex necessitatibus praesentium sint quis exercitationem, labore quisquam qui magnam laboriosam provident.</p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="home pa-5">
+<h1>Home oage</h1>
+</div>
+</template>
+
+<script>
+ 
+</script>
