f300fcddab76ef6ef0030c42404845aeef0f8a48
MichaelaTarryn <mward7006@gmail.com>
2022-09-28 11:24:56 +0200

    done

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,22 +1,54 @@
 <template>
 <div class="home">
+  <v-text-field
+  v-model="newTaskTitle"
+  @click:append="addTask"
+  @keyup.enter="addTask"
+  class="pa-3"
+            outlined
+            label="Add Task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
   <v-list
   class="pt-0"
       flat
+    >
+    <div
+     v-for="task in tasks" 
+    :key="task.id"
 
     >
-    <div v-for="task in tasks" :key="tasks.id">
-       <v-list-item >
-          <template v-slot:default="{ active }">
+       <v-list-item 
+        @click="doneTask(task.id)"
+        :class="{'blue lighten-5' : task.done}"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox :input-value="task.done"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{task.title}}</v-list-item-title>
+              <v-list-item-title
+              :class="{'text-decoration-line-through':task.done}"
+              >
+
+                {{task.title}}
+              </v-list-item-title>
               
             </v-list-item-content>
+             <v-list-item-action>
+        
+          <v-btn 
+          @click.stop="deleteTask(task.id)"
+          icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
           </template>
+          
+       
         </v-list-item>
         <v-divider></v-divider>
     </div>
@@ -34,30 +66,50 @@ import { fa } from 'vuetify/lib/locale';
   export default{
 data(){
   return{
+    newTaskTitle:'',
      tasks:[
-      {
-        id:1,
-        title:'Wake up' ,
-        done:false,
-      },
-      {
-        id:2,
-        title:'Get up' ,
-        done:false,
-      },
-      {
-        id:3,
-        title:'Brush teeth' ,
-        done:false,
-      },
-      {
-        id:4,
-        title:'SMILE' ,
-        done:false,
-      }
+      // {
+      //   id:1,
+      //   title:'Wake up' ,
+      //   done:false,
+      // },
+      // {
+      //   id:2,
+      //   title:'Get up' ,
+      //   done:false,
+      // },
+      // {
+      //   id:3,
+      //   title:'Brush teeth' ,
+      //   done:false,
+      // },
+      // {
+      //   id:4,
+      //   title:'SMILE' ,
+      //   done:false,
+      // }
      ]
   }
  
+},
+methods:{
+  addTask(){
+    let newTask ={
+      id:Date.now(),
+      title:this.newTaskTitle,
+      done:false
+    }
+    this.tasks.push(newTask)
+    this.newTaskTitle=''
+  },
+  doneTask(id){
+    let task =this.tasks.filter(task => task.id === id)[0]
+    task.done =!task.done
+  },
+  deleteTask(id){
+  this.tasks=this.tasks.filter(task => task.id !==id)
+  }
+
 }
   }
 
